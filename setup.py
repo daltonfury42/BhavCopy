8df8ffc910c0ef64a78f15e09d11a55433bd64df
Dalton Fury <daltonfury42@disroot.org>
2019-02-16 00:37:40 +0530

    Initial project setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='bhavcopy',
+      version='0.1',
+      description='A simple python script that fetches daily Bhav Copy data from BSE website and serves the data on a website',
+      url='http://github.com/daltonfury42/bhavcopy',
+      author='Dalton Fury',
+      author_email='daltonfury42@disroot.org',
+      license='MIT',
+      packages=['bhavcopy'],
+      install_requires=[],
+      zip_safe=False)
